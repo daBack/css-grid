8749f4242b26b90bebfccf476f037f41d71f1567
daBack <oscar.back@cybercom.com>
2020-11-17 21:47:35 +0100

    Challenge complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='grid'>
+
+      <div className='logo'>
+        <h1>Totally Tubular</h1>
+      </div>
+
+      <div className='nav'>
+        <p>MIKE</p>
+        <p>DUSTIN</p>
+        <p>LUCAS</p>
+        <p>WILL</p>
+      </div>
+
+      <img 
+        className='quoteImg'
+        src='https://i.ibb.co/3WPy4hF/asd.png' 
+        alt='title' />
+
+      <img
+        className='image'
+        src='https://pbs.twimg.com/profile_images/762638100417814528/0HwN79Hv_400x400.jpg'
+        alt='Dustin'
+      />
+
+      <div className='text'>
+        <p>Dustin is a cool dude who always has his cap on. </p>
+        <p>The legend says that he has the most sexy taunt man could ever acknowledged.</p>
+      </div>
+
+      <div className='rawr'>
+        “RRRRRrrrrrrrrrrrrr”
+      </div>
+
+
     </div>
   );
 }
